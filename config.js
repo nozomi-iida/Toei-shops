b102cf0d58bef46a0ecf135cf250a99c946ec5a8
nozomi-iida <iida19990106@gmail.com>
2021-03-21 17:52:45 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,3 @@
+// This is client side config only - don't put anything in here that shouldn't be public!
+export const endpoint = `https://ecomm-yoql-prod.herokuapp.com/`;
+export const perPage = 4;
